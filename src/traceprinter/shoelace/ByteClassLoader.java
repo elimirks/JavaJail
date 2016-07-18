793b04df14d54ea7195e5ccbf0403bc2a5965897
Elijah Mirecki <elimirks@gmail.com>
2016-07-18 11:19:10 -0400

    More meaningful error reporting for ommitted info.
    It will now report exactly what went wrong if the VM exists due to an
    invalid invocation. For instance, if the main() method is private, it
    will dump a message saying to make the main() class public.
    See VMCommandee.java for all reporting messages.

--- src/traceprinter/shoelace/ByteClassLoader.java
+++ src/traceprinter/shoelace/ByteClassLoader.java
@@ -1,12 +1,12 @@
 package traceprinter.shoelace;
 import java.util.*;
+
+/**
+ * A class loader that uses bytecode directly.
+ *
+ * Based on: http://stackoverflow.com/questions/1781091/
+ */
 public class ByteClassLoader extends ClassLoader {
-
-/*** 
- A class loader that uses bytecode directly
- Based on: http://stackoverflow.com/questions/1781091/
-***/
-
     static ByteClassLoader instance;
 
     private TreeMap<String, byte[]> definitions = new TreeMap<>();
@@ -23,18 +23,13 @@ public class ByteClassLoader extends ClassLoader {
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         byte[] bytecode = definitions.get(name);
         if (bytecode == null) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("Asked ByteClassLoader for undefined class " + name);
-            sb.append(" --- Known: ");
-            for (String S : definitions.keySet())
-                sb.append(S+" ");
-            throw new RuntimeException(sb.toString());
+            throw new ClassNotFoundException(sb.toString());
         }
-        return defineClass(name, bytecode, 0, bytecode.length); 
+        return defineClass(name, bytecode, 0, bytecode.length);
     }
 
     public static Class<?> publicFindClass(final String name) throws ClassNotFoundException {
         return instance.findClass(name);
     }
-
 }
+
--- src/traceprinter/shoelace/VMCommandee.java
+++ src/traceprinter/shoelace/VMCommandee.java
@@ -12,44 +12,24 @@ import java.lang.reflect.*;
  * VMCommandee is in the debugee.
  */
 public class VMCommandee {
-    private static final String MAIN_CLASS_NOT_FOUND_MESSAGE =
-        "Internal error: main class %s not found";
-    private static final String MAIN_METHOD_NOT_FOUND_MESSAGE =
-        "Class '%s' requires a 'public static void main' method";
-
-    // Returns null if everything worked
-    // else, returns an error message
+    /**
+     * Runs the main method in the users code.
+     *
+     * @param className The "main class" of the user code.
+     * @param args The args to pass to the users main method.
+     * @param stdin The STDIN to stream into the user program.
+     * @return An error message, or null if everything worked
+     */
     public String runMain(String className, String[] args, String stdin) {
-        Class<?> target;
         try {
-            target = ByteClassLoader.publicFindClass(className);
-        } catch (ClassNotFoundException e) {
-            return String.format(MAIN_CLASS_NOT_FOUND_MESSAGE, className);
-        }
+            Class<?> target = targetFromClassName(className);
+            Method main = mainMethodFromClass(target, className);
+            setStdin(stdin);
 
-        Method main;
-        try {
-            main = target.getMethod("main", new Class[]{String[].class});
-        } catch (NoSuchMethodException e) {
-            return String.format(MAIN_METHOD_NOT_FOUND_MESSAGE, className);
-        }
-
-        if (stdin != null) {
-            try {
-                System.setIn(new ByteArrayInputStream(stdin.getBytes("UTF-8")));
-            } catch (SecurityException | UnsupportedEncodingException e) {
-                return "Internal error: can't setIn";
-            }
-        }
-
-        int modifiers = main.getModifiers();
-        if (modifiers != (Modifier.PUBLIC | Modifier.STATIC)) {
-            return String.format(MAIN_METHOD_NOT_FOUND_MESSAGE, className);
-        }
-
-        try {
-            // First is null since it is a static method
-            main.invoke(null, new Object[]{args});
+            // Instance param is null since it is a static method
+            main.invoke(null, new Object[] {
+                args
+            });
             return null;
         } catch (IllegalAccessException e) {
             return "Internal error invoking main";
@@ -67,6 +47,87 @@ public class VMCommandee {
 
             //if (e.getTargetException() instanceof Error)
             //  throw (Error)(e.getTargetException());
+        } catch (CodeStagingException e) {
+             return e.getMessage();
+        }
+    }
+
+    /**
+     * Finds the user class with the given name.
+     */
+    private Class<?> targetFromClassName(String className)
+            throws CodeStagingException {
+        Class<?> target;
+        try {
+            target = ByteClassLoader.publicFindClass(className);
+        } catch (ClassNotFoundException e) {
+            throw new CodeStagingException(String.format(
+                "Main class '%s' not found.",
+                className));
+        }
+
+        int classMod = target.getModifiers();
+        if ((classMod & Modifier.PUBLIC) != Modifier.PUBLIC) {
+            throw new CodeStagingException(String.format(
+                "Class '%s' must be public.",
+                className));
+        } else if ((classMod & Modifier.INTERFACE) == Modifier.INTERFACE) {
+            throw new CodeStagingException(String.format(
+                "'%s' must be a class, not an interface.",
+                className));
+        }
+        return target;
+    }
+
+    /**
+     * Finds the main method, ready to invoke, from the given class.
+     */
+    private Method mainMethodFromClass(Class<?> target, String className)
+            throws CodeStagingException {
+        Method main;
+        try {
+            main = target.getDeclaredMethod("main", String[].class);
+        } catch (NoSuchMethodException e) {
+            throw new CodeStagingException(String.format(
+                "Class '%s' requires a 'public static void main' method.",
+                className));
+        }
+
+        int mainModifiers = main.getModifiers();
+        if ((mainModifiers & Modifier.PUBLIC) != Modifier.PUBLIC) {
+            throw new CodeStagingException(String.format(
+                "The main method in '%s' must be public.",
+                className));
+        } else if ((mainModifiers & Modifier.STATIC) != Modifier.STATIC) {
+            throw new CodeStagingException(String.format(
+                "The main method in '%s' must be static.",
+                className));
+        }
+
+        return main;
+    }
+
+    /**
+     * Sets user code stdin (inside the VM).
+     */
+    private void setStdin(String stdin) throws CodeStagingException {
+        if (stdin == null) return;
+
+        try {
+            System.setIn(new ByteArrayInputStream(stdin.getBytes("UTF-8")));
+        } catch (SecurityException | UnsupportedEncodingException e) {
+            throw new CodeStagingException("Internal error: can't set STDIN");
+        }
+    }
+
+    /**
+     * Exception class used to signify user code misconfiguration.
+     *
+     * For instance, a missing main() method.
+     */
+    private class CodeStagingException extends Exception {
+        public CodeStagingException(String message) {
+            super(message);
         }
     }
 }
